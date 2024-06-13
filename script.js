8172a3dc44344a3f5baa675fbd76ae89fbd8ba1d
FarflungEyestrain <stedgegavin@gmail.com>
2024-06-13 14:17:56 -0400

    dsdsds

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let image1Button = document.querySelector('.option-one');
 let image2Button = document.querySelector('.option-two');
 let image3Button = document.querySelector('.option-three');
 let image4Button = document.querySelector('.option-four');
-
-
+let image5Button = document.querySelector('.option-five');
+let screenOne = document.querySelector('.story-opening');
+let screenTwo = document.querySelector('.option-one-screen');
+let screenThree = document.querySelector('.option-two-screen');
+let screenFour = document.querySelector('.option-three-screen');
+let screenFive = document.querySelector('.option-four-screen');
+let screenSix = document.querySelector('.option-five-screen');
 
 
 
@@ -14,14 +19,39 @@ let image4Button = document.querySelector('.option-four');
 
 
 image1Button.addEventListener('click', function(){
+    image2Button.style.display = "block";
+    image1Button.style.display = "none";
+    screenTwo.style.display = "block";
+    screenOne.style.display = "none";
     
 });
 
 image2Button.addEventListener('click', function(){
-
+    image3Button.style.display = "block";
+    image2Button.style.display = "none";
+    screenThree.style.display = "block";
+    screenTwo.style.display = "none";
 });
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+image3Button.addEventListener('click', function(){
+    image4Button.style.display = "block";
+    image5Button.style.display = "block";
+    image3Button.style.display = "none";
+    screenFour.style.display = "block";
+    screenThree.style.display = "none";
+});
 
-// });
+image4Button.addEventListener('click', function(){
+    image4Button.style.display = "none";
+    image5Button.style.display = "none";
+    screenFive.style.display = "block";
+    screenFour.style.display = "none";
+});
+
+image5Button.addEventListener('click', function(){
+    image4Button.style.display = "none";
+    image5Button.style.display = "none";
+    screenSix.style.display = "block";
+    screenFour.style.display = "none";
+});
